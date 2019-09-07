8283154c297055747639ea8608310c4b9450af6f
Felipe Paolozze <fpaolozze@gmail.com>
2019-09-07 00:30:03 -0300

    updating deps

--- clj/project.clj
+++ clj/project.clj
@@ -3,12 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [cheshire "5.8.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [cheshire "5.9.0"]
                  [com.walmartlabs/lacinia "0.34.0"]
-                 [org.apache.logging.log4j/log4j-slf4j-impl "2.11.1"]
-                 [org.apache.logging.log4j/log4j-api "2.11.1"]
-                 [org.apache.logging.log4j/log4j-core "2.11.1"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.12.1"]
+                 [org.apache.logging.log4j/log4j-api "2.12.1"]
+                 [org.apache.logging.log4j/log4j-core "2.12.1"]
                  [com.walmartlabs/lacinia-pedestal "0.12.0"]]
   :main ^:skip-aot clj.core
   :target-path "target/%s"
